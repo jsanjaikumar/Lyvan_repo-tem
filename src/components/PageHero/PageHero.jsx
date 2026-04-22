97f2ee9ba55308daa8706bd80811aa886647d925
Sanjai Kumar <youremail@example.com>
2026-04-22 14:38:43 +0530

    final fix before push

--- src/components/PageHero/PageHero.jsx
+++ src/components/PageHero/PageHero.jsx
@@ -26,7 +26,10 @@ export default function PageHero({ variant, title, breadcrumb }) {
           <nav className="breadcrumbs d-inline-block">
             <ol className="list-unstyled d-flex gap-2 mb-0 justify-content-center">
               <li>
-                <Link to="/" className="text-white-50 text-decoration-none hover-white">
+                <Link
+                  to="/"
+                  className="text-white-50 text-decoration-none hover-white"
+                >
                   Home
                 </Link>
               </li>
